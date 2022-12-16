6c5ba0d7ad487e4fe68dfc02719e8442481e7f2b
Pascual Cano <pascual@allhuman.com>
2022-12-16 10:59:52 +0100

    Going functional

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -1,57 +1,57 @@
 import { TennisGame } from "./TennisGame";
 
 export class TennisGame1 implements TennisGame {
-	private m_score1: number = 0;
-	private m_score2: number = 0;
+	private score1: number = 0;
+	private score2: number = 0;
 
 	wonPoint(playerName: string): void {
-		if (playerName === "player1") this.m_score1 += 1;
-		else this.m_score2 += 1;
+		if (playerName === "player1") this.score1 += 1;
+		else this.score2 += 1;
 	}
 
 	getScore(): string {
-		return TennisGame1.getScore(this.m_score1, this.m_score2);
-	}
-
-	private static getScore(score1: number, score2: number): string {
-		if (score1 === score2) {
-			return TennisGame1.getScoreWhenTie(score1);
-		} else if (score1 >= 4 || score2 >= 4) {
-			return TennisGame1.getScoreForAdvantagesAndWins(score1, score2);
-		} else {
-			return TennisGame1.getScoresBelowDeuce(score1, score2);
-		}
-	}
-
-	private static getScoresBelowDeuce(score1: number, score2: number) {
-		return `${TennisGame1.getPointsAsString(score1)}-${TennisGame1.getPointsAsString(score2)}`;
-	}
-
-	private static getScoreForAdvantagesAndWins(score1: number, score2: number) {
-		const minusResult: number = score1 - score2;
-		if (minusResult === 1) return "Advantage player1";
-		else if (minusResult === -1) return "Advantage player2";
-		else if (minusResult >= 2) return "Win for player1";
-		else return "Win for player2";
-	}
-
-	private static getScoreWhenTie(points: number) {
-		if (points > 2) return "Deuce";
-		else return `${TennisGame1.getPointsAsString(points)}-All`;
-	}
-
-	private static getPointsAsString(points: number) {
-		switch (points) {
-			case 0:
-				return "Love";
-			case 1:
-				return "Fifteen";
-			case 2:
-				return "Thirty";
-			case 3:
-				return "Forty";
-			default:
-				return "";
-		}
+		return getScore(this.score1, this.score2);
 	}
 }
+
+const getScore = (score1: number, score2: number): string => {
+	if (score1 === score2) {
+		return getScoreWhenTie(score1);
+	} else if (score1 >= 4 || score2 >= 4) {
+		return getScoreForAdvantagesAndWins(score1, score2);
+	} else {
+		return getScoresBelowDeuce(score1, score2);
+	}
+};
+
+const getScoresBelowDeuce = (score1: number, score2: number) => {
+	return `${getPointsAsString(score1)}-${getPointsAsString(score2)}`;
+};
+
+const getScoreForAdvantagesAndWins = (score1: number, score2: number) => {
+	const minusResult: number = score1 - score2;
+	if (minusResult === 1) return "Advantage player1";
+	else if (minusResult === -1) return "Advantage player2";
+	else if (minusResult >= 2) return "Win for player1";
+	else return "Win for player2";
+};
+
+const getScoreWhenTie = (points: number) => {
+	if (points > 2) return "Deuce";
+	else return `${getPointsAsString(points)}-All`;
+};
+
+const getPointsAsString = (points: number) => {
+	switch (points) {
+		case 0:
+			return "Love";
+		case 1:
+			return "Fifteen";
+		case 2:
+			return "Thirty";
+		case 3:
+			return "Forty";
+		default:
+			return "";
+	}
+};
