6fa3baccd67d8e5bfcc114216d6b3c90f058fc0a
Pascual Cano <pascual@allhuman.com>
2022-12-16 11:54:42 +0100

    Remove player names

--- tests/TennisGame1.spec.ts
+++ tests/TennisGame1.spec.ts
@@ -6,7 +6,7 @@ const scores = getAllScores();
 describe("TennisGame1", () => {
 	scores.forEach(([player1Score, player2Score, expectedScore]) => {
 		it(`scores ${player1Score}:${player2Score} as ${expectedScore}`, () => {
-			checkScore(new TennisGame1("player1", "player2"), player1Score, player2Score, expectedScore);
+			checkScore(new TennisGame1(), player1Score, player2Score, expectedScore);
 		});
 	});
 });
